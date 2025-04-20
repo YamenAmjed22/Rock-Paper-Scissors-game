3d72660c924fa40ac5ec1eac4ac9048c577a0c4d
YamenAmjed22 <yamenamjed22@gmail.com>
2025-04-19 23:58:44 -0900

    i am just add 3 sout statment to make learn on githup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello in my game !");
+        System.out.println("Be ready for play");
+        System.out.println("Go to RockPaperScissors class and run it to play the game ");
+
+
+
     }
 }
